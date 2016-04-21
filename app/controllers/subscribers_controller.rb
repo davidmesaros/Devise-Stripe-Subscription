736e1802cc3e1d5e47063fd080263d127b46cccd
davidmesaros <david.mesaros@hotmail.com>
2016-04-21 22:30:21 +1000

    added stripe susbscribtion

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,24 @@
+class SubscribersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new
+  end
+
+  def update
+    token = params[:stripToken]
+
+    customer = Stripe::Customer.create(
+        card: token,
+        plan: 999,
+        email: current_user.email
+        )
+
+    current_user.subscribed = true
+   
+    current_user.stripeid = customer.id
+    current_user.save
+
+    redirect_to users_path
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  devise_for :users, :controllers => { :registrations => 'registrations' }
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :users
+  resources :subscribers
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
